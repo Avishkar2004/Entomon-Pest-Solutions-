8af93f372065b1bd3613e7c1b893f3ab5408f4ca
Avishkar2004 <avishkarkakde2004@gmail.com>
2023-10-20 14:13:56 +0530

    hi

---
+++ public/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import axios from 'axios';
+
+
+axios.defaults.withCredentials = true; // even for get requests if
+                                    // demand session authentication
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'x-csrftoken';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
