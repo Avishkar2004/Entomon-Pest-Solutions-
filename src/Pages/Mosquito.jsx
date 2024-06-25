299597e5924c54c4f7d4de0c8a79e42781e17fde
Avishkar2004 <avishkarkakde2004@gmail.com>
2024-06-25 11:24:48 +0530

    Mosquito

--- src/Pages/Mosquito.jsx
+++ src/Pages/Mosquito.jsx
@@ -84,6 +84,13 @@ const Mosquito = () => {
 
   const scrollToTop = () => {
     window.scrollTo({
+      top: 550,
+      behavior: "smooth",
+    });
+  };
+
+  const scrollToTopBook = () => {
+    window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
@@ -189,7 +196,7 @@ const Mosquito = () => {
           <div className="lg:w-1/2 p-8 lg:p-12 text-center lg:text-center">
             <Link
               to="/contact"
-              onClick={scrollToTop}
+              onClick={scrollToTopBook}
               className="bg-indigo-600 hover:bg-indigo-700 text-white text-lg py-3 px-8 rounded-xl focus:outline-none focus:ring focus:ring-indigo-300 transition duration-300"
             >
               Book Now
@@ -390,9 +397,9 @@ const Mosquito = () => {
             >
               For further details or to schedule an inspection, please contact
               us today. We are here to help.{" "}
-              <a href="/" className="text-blue-300 underline">
+              <Link onClick={scrollToTop} to="" className="text-blue-300 underline">
                 call us
-              </a>
+              </Link>
             </p>
           </div>
         </div>
