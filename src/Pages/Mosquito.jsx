21597e6ee0d2de6cc156dfc0774f53ca1dfa8f47
Avishkar2004 <avishkarkakde2004@gmail.com>
2024-06-27 11:14:42 +0530

    Mosquito scoroll to top

--- src/Pages/Mosquito.jsx
+++ src/Pages/Mosquito.jsx
@@ -519,7 +519,7 @@ const Mosquito = () => {
           <div className='grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 gap-4'>
             {locations.map((location) => (
               <ul key={location}>
-                <Link className="text-blue-700 text-sm" to={`/mos/${location}`}>
+                <Link onClick={scrollToTopBook} className="text-blue-700 text-sm" to={`/mos/${location}`}>
                   Mosquito Control in {location.split('-').map(word => word.charAt(0, 2).toUpperCase() + word.slice(1)).join(", ")}
                 </Link>
               </ul>
