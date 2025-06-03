6ef67793e0debaa0748227b762eed5fdea43349b
Avishkar2004 <avishkarkakde2004@gmail.com>
2025-06-03 20:11:55 +0530

    Improved Performance

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import axios from 'axios';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import axios from "axios";
 
 axios.defaults.withCredentials = true; // even for get requests if
-                                    // demand session authentication
-axios.defaults.xsrfCookieName = 'csrftoken';
-axios.defaults.xsrfHeaderName = 'x-csrftoken';
+// demand session authentication
+axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.xsrfHeaderName = "x-csrftoken";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
